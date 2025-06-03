b862929252cc4a190c4743612d6f1d7f9b58917a
Aliaksei Mazheika <aleksmozheiko94@gmail.com>
2025-06-03 14:32:59 +0100

    Update userSettingsPage

--- src/pages/UserSettingsPage.vue
+++ src/pages/UserSettingsPage.vue
@@ -46,14 +46,9 @@
             </q-item-section>
             <q-item-section side>
               <q-toggle
-                :model-value="userStore.preferences.arePushNotificationsEnabled"
-                @update:model-value="
-                  (value) =>
-                    userStore.updateUserProfile({
-                      preferences: { pushNotificationsEnabled: value },
-                    })
-                "
                 color="primary"
+                :model-value="userStore.preferences.arePushNotificationsEnabled"
+                @update:model-value="updatePreference('pushNotificationsEnabled', $event)"
               />
             </q-item-section>
           </q-item>
@@ -69,19 +64,17 @@
             <q-item-section>
               <q-item-label class="text-primary">Dark Mode</q-item-label>
               <q-item-label
-                caption
                 class="text-caption"
+                caption
               >
                 Toggle between light and dark theme
               </q-item-label>
             </q-item-section>
             <q-item-section side>
               <q-toggle
-                :model-value="userStore.preferences.isDark"
                 color="primary"
-                @update:model-value="
-                  (value) => userStore.updateUserProfile({ preferences: { darkMode: value } })
-                "
+                :model-value="userStore.preferences.isDark"
+                @update:model-value="updatePreference('darkMode', $event)"
               />
             </q-item-section>
           </q-item>
@@ -158,12 +151,18 @@ const router = useRouter()
 
 const isSigningOut = ref(false)
 
+function updatePreference(preferenceKey: 'pushNotificationsEnabled' | 'darkMode', value: boolean) {
+  userStore.updateUserProfile({
+    preferences: { [preferenceKey]: value },
+  })
+}
+
 async function handleSignOut() {
   isSigningOut.value = true
 
   await userStore.signOut()
-
   await router.push('/auth')
+
   isSigningOut.value = false
 }
 </script>
