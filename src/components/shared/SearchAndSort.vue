d4bf63ec8d273ee71bb3fc323da68ab1c0d4efe5
Aliaksei Mazheika <aleksmozheiko94@gmail.com>
2025-09-30 14:52:47 +0100

    Updated search and sort for mobile

--- src/components/shared/SearchAndSort.vue
+++ src/components/shared/SearchAndSort.vue
@@ -5,11 +5,15 @@
     bordered
   >
     <q-card-section>
-      <div class="row items-center q-col-gutter-md">
-        <div class="col-12 col-sm-9">
+      <div
+        class="row items-center"
+        :class="$q.screen.lt.md ? 'q-col-gutter-sm' : 'q-col-gutter-md'"
+      >
+        <div class="col col-sm-9">
           <q-input
             :model-value="searchQuery"
             :placeholder="searchPlaceholder"
+            :dense="$q.screen.lt.md"
             debounce="300"
             outlined
             clearable
@@ -21,15 +25,28 @@
             </template>
           </q-input>
         </div>
-        <div class="col-12 col-sm-3">
+        <div class="col-auto col-sm-3">
           <q-select
             :model-value="sortBy"
             :options="sortOptions"
-            label="Sort by"
+            :label="sortLabel"
+            :dense="$q.screen.lt.md"
+            :display-value="displayValue"
+            :hide-dropdown-icon="$q.screen.lt.md"
             outlined
             emit-value
             @update:model-value="emit('update:sortBy', $event)"
-          />
+          >
+            <template
+              v-if="$q.screen.lt.md"
+              #prepend
+            >
+              <q-icon
+                class="q-pl-xs"
+                name="eva-swap-outline"
+              />
+            </template>
+          </q-select>
         </div>
       </div>
     </q-card-section>
@@ -37,6 +54,9 @@
 </template>
 
 <script setup lang="ts">
+import { useQuasar } from 'quasar'
+import { computed } from 'vue'
+
 type SortOption = {
   label: string
   value: string
@@ -58,4 +78,12 @@ withDefaults(
     searchPlaceholder: 'Search...',
   },
 )
+
+const $q = useQuasar()
+
+// Hide the label on mobile screens to make the sort select more compact
+const sortLabel = computed(() => ($q.screen.lt.md ? undefined : 'Sort by'))
+
+// Hide the display value on mobile to show only the icon
+const displayValue = computed(() => ($q.screen.lt.md ? ' ' : undefined))
 </script>
